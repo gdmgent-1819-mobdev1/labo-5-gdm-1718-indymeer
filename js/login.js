96ecdebc5b28dd08ce6fc52840fa559a66e676ae
gdm-1718-indymeer <indymeer@student.arteveldehs.be>
2018-10-29 16:33:01 +0100

    added login/signup with working form

---
+++ js/login.js
@@ -0,0 +1,128 @@
+/*************************************************************************************************
+ * https://stackoverflow.com/questions/39349671/how-to-maintain-firebase-auth-for-multiple-pages-on-the-web
+ * 
+ * 
+ */
+(function () {
+  var el = document.getElementById('btn_signup');
+if(el){
+  el.addEventListener('click', signup, false);
+}
+var ee = document.getElementById('btn_login');
+if(ee){
+  el.addEventListener('click', login, false);
+}
+  requestNotificationPermission();
+})();
+
+function signup(e) {
+  e.preventDefault();
+
+  let email = document.getElementById("signup_email").value;
+  let password = document.getElementById("signup_password").value;
+
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then(function (response) {
+    window.location = "pages/blog.html";
+   // let createButton = document.getElementById('plus');
+  //  createButton.style.cssText = "opacity: 1; z-index: 110;";
+    sendNotification('Thanks for signing up to our website! Check your e-mail for account verification!');
+    sendVerificationEmail(response.user);
+  })
+    .catch(function (error) {
+    // Handle Errors here.
+    let errorCode = error.code;
+    let errorMessage = error.message;
+
+    console.log(errorCode, errorMessage);
+    document.getElementById('signup_error').innerHTML = errorCode + " - " + errorMessage;
+  });
+}
+
+function login(e) {
+  e.preventDefault();
+
+  let email = document.getElementById("login_email").value;
+  let password = document.getElementById("login_password").value;
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function (response) {
+   // window.location = "pages/blog.html";
+    sendNotification('You are now logged in successfully!');
+    showUserInfo(response.user);
+    
+    //let createButton = document.getElementById('plus');
+    //createButton.style.cssText = "opacity: 1; z-index: 110;";
+
+  })
+    .catch(function (error) {
+    // Handle Errors here.
+    let errorCode = error.code;
+    let errorMessage = error.message;
+
+    console.log(errorCode, errorMessage);
+    document.getElementById('login_error').innerHTML = errorCode + " - " + errorMessage;
+  });
+}
+
+function sendVerificationEmail(user) {
+  user.sendEmailVerification()
+    .then(function () {
+    // Email sent.
+  }).catch(function (error) {
+    // Handle Errors here.
+    let errorCode = error.code;
+    let errorMessage = error.message;
+
+    console.log(errorCode, errorMessage);
+  });
+}
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+   window.location = 'pages/blog.html';
+   createButton.style.cssText = "opacity: 1; z-index: 110;";
+
+  }
+});
+
+function sendNotification(msg) {
+  let notif = new Notification(msg);
+}
+
+function requestNotificationPermission() {
+  if (Notification && Notification.permission === 'default') {
+    Notification.requestPermission(function (permission) {
+      if (!('permission' in Notification)) {
+        Notification.permission = permission;
+      }
+    });
+  }
+}
+
+function showUserInfo(user) {
+  document.getElementById('user_info').innerHTML = "<h1> Welcome " + user.email + " ! </h1>";
+}
+
+/**
+ * Variables
+ */
+const signupButton = document.getElementById('signup-button'),
+    loginButton = document.getElementById('login-button'),
+    userForms = document.getElementById('user_options-forms')
+
+/**
+ * Add event listener to the "Sign Up" button
+ */
+signupButton.addEventListener('click', () => {
+  userForms.classList.remove('bounceRight')
+  userForms.classList.add('bounceLeft')
+}, false)
+
+/**
+ * Add event listener to the "Login" button
+ */
+loginButton.addEventListener('click', () => {
+  userForms.classList.remove('bounceLeft')
+  userForms.classList.add('bounceRight')
+}, false)
---
+++ js/main.js
@@ -0,0 +1,96 @@
+
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+
+let createPostButton = document.getElementById('plus');
+let submit = document.getElementById('saveForm');
+let form = document.getElementById('activityform');
+
+
+
+function closest(e, t){ 
+    return !e? false : e === t ? true : closest(e.parentNode, t);
+  }
+
+createPostButton.addEventListener("click", function(e){
+    document.querySelector('#activityform').style.cssText="height: 500px; padding: 20px; opacity: 1; z-index: 110;"; 
+    createPostButton.disabled = true;
+    e.stopPropagation();
+  
+});
+
+document.body.addEventListener("click", function(e) {
+    if (!closest(e.target, form)) {
+        form.style.display = "none";
+        open.disabled = false;
+    }
+});
+
+
+// SUBMIT FORM BUTTON: RESET FORM AND HIDE THE FORM
+
+submit.addEventListener("click", function(e){
+    e.preventDefault();
+    console.log('button clicked');
+    createItems();
+    document.getElementById("newActivity").reset();
+    form.style.display = "none";
+    });
+
+const ul = document.querySelector('#feed');
+
+function createItems(){
+
+    // GENERATE DIVS FOR CONTAINERS
+    let container = createNode("div");
+    let iconHolder = createNode("div");
+    let icon = createNode("div");
+    let textHolder = createNode("div");
+    let title = createNode("div");
+    let description = createNode("div");
+    let postOption = createNode("div");
+    let tools = createNode("div");
+    let ellipsis = createNode("i");
+
+    // CLASSNAMES 
+
+    container.className = "feed-item";
+    iconHolder.className = "icon-holder";
+    icon.className = "icon";
+    textHolder.className = "text-holder col-3-5";
+    title.className = "feed-title";
+    description.className = "feed-description";
+    postOption.className = "post-options-holder";
+    tools.className = "tools";
+    ellipsis.className = "fa fa-ellipsis-v";
+    ellipsis.id = "postsettings"
+
+    
+    
+    append(tools, ellipsis);
+    append(postOption, tools);
+    append(textHolder, title);
+    append(textHolder, description);
+    append(container, textHolder);
+    append(iconHolder, icon);
+    append(container, postOption);
+    append(container, iconHolder);
+    append(ul, container);
+
+
+    
+    title.innerHTML = document.querySelector('#activityTitle').value;
+    description.innerHTML =  document.querySelector('#activityDescription').value;
+
+    
+}
+
+
+
+
+
